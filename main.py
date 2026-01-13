ea752010c354b5046414102c70cdd87d63573f87
kurosawachzzk <kurosawachzzk@gmail.com>
2026-01-13 18:32:23 +0900

    게산기 프로그램 입니다

---
+++ main.py
@@ -0,0 +1,11 @@
+print("계산할 두 수를 입력해주세요>>>")
+a1 = float(input("첫번째 수"))
+a2 = float(input("두번째 수"))
+plus = a1 + a2
+minus = a1 - a2
+multi = a1 * a2
+div = a1 / a2
+print("두 수를 더한 값 : ", plus)
+print("두 수를 뺀 값 : ", minus)``
+print("두 수를 곱한 값 : ", multi)
+print("두 수를 나눈 값 : ", div)
